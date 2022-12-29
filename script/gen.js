b668c167f8de1de35cb03990ecf4e676fbd7c8dc
rui_er <qz-cqy@outlook.com>
2022-12-29 22:02:07 +0800

    add sample test

---
+++ script/gen.js
@@ -0,0 +1,32 @@
+function choice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+function generate() {
+    let url = "../data/dict.json";
+    let request = new XMLHttpRequest();
+    let result = 'error';
+    request.open("get", url);
+    request.send(null);
+    request.onload = function () {
+        if (request.status == 200) {
+            let json = JSON.parse(request.responseText);
+            let subject = choice(json['subject']);
+            let predicate = choice(json['predicate']);
+            let object = choice(json['object']);
+            let attribute = choice(json['attribute']);
+            let adverbial_1 = choice(json['adverbial-1']);
+            let adverbial_2 = choice(json['adverbial-2']);
+            result = `
+                    <div class='adverbial'>${adverbial_1}</div>
+                    ，
+                    <div class='subject'>${subject}</div>
+                    <div class='adverbial'>${adverbial_2}</div>
+                    <div class='predicate'>${predicate}</div>
+                    <div class='attribute'>${attribute}</div>
+                    <div class='object'>${object}</div>
+                    。
+                    `;
+        }
+    }
+    return result;
+}
--- script/main.js
+++ script/main.js
@@ -1,8 +1,8 @@
 function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
+    let query = window.location.search.substring(1);
+    let vars = query.split("&");
+    for (let i = 0; i < vars.length; i++) {
+        let pair = vars[i].split("=");
         if (pair[0] == variable) { return pair[1]; }
     }
     return '-1';
@@ -11,9 +11,9 @@ function setStyle(name, stylename, styleval) {
     document.getElementsByTagName(name)[0].style[stylename] = styleval;
 }
 (function () {
-    var date = new Date();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
+    let date = new Date();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
     if (month == 12 && day == 13) {
         setStyle("html", "-webkit-filter", "grayscale(100%)");
         setStyle("html", "-moz-filter", "grayscale(100%)");
