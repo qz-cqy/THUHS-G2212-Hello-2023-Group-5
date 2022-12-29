452c1e8243796beb1f6dc68a3bcc15d136deb6d0
rui_er <qz-cqy@outlook.com>
2022-12-29 22:18:14 +0800

    fix

--- script/gen.js
+++ script/gen.js
@@ -2,9 +2,8 @@ function choice(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 function generate() {
-    let url = "https://blog.ak-ioi.cf/THUHS-G2212-Hello-2023-Group-5/data/dict.json";
+    let url = "./data/dict.json";
     let request = new XMLHttpRequest();
-    let result = 'error';
     request.open("get", url);
     request.send(null);
     request.onload = function () {
@@ -17,7 +16,7 @@ function generate() {
             let attribute = choice(json['attribute']);
             let adverbial_1 = choice(json['adverbial-1']);
             let adverbial_2 = choice(json['adverbial-2']);
-            result = `
+            let html = `
                     <div class='adverbial'>${adverbial_1}</div>
                     ，
                     <div class='subject'>${subject}</div>
@@ -27,9 +26,7 @@ function generate() {
                     <div class='object'>${object}</div>
                     。
                     `;
-            console.log(result);
+            document.getElementById("sentence").innerHTML = html;
         }
     }
-    console.log(result);
-    return result;
 }
